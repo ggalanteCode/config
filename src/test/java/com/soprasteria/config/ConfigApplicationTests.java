dab033eb449f032bf72e3c8ee62a52b04df3f98d
GALANTE Giovanni <giovanni.galante@soprasteria.com>
2022-12-19 16:55:14 +0100

    Spring cloud config server example

---
+++ src/test/java/com/soprasteria/config/ConfigApplicationTests.java
@@ -0,0 +1,13 @@
+package com.soprasteria.config;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
